bcb0664a4730386f1b8e80821fb1c87288e1eadc
mobilereal <suchanat.l@ku.th>
2021-03-16 17:20:45 +0700

    add class

--- main.py
+++ main.py
@@ -113,5 +113,36 @@ def create_books(req_body: createBookPayload):
     return JSONResponse(content=response, status_code=201)
 
 
+class updateBookPayload(BaseModel):
+    name: str = ""
+    page: int = 0
+
+
+@app.patch("/books/{book_id}")
+def update_book_by_id(req_body: updateBookPayload, book_id: str):
+    req_body_dict = req_body.dict()
+
+    name = req_body_dict["name"]
+    page = req_body_dict["page"]
+
+    print("[ Log ] books_id", book_id)
+    print("[ Log ] name", name)
+    print("[ Log ] page", page)
+
+    update_message = f"Update book id {book_id} is complete !! "
+    response = {"status": "ok", "data": update_message}
+    return JSONResponse(content=response, status_code=200)
+
+
+@app.delete("/books/{book_id}")
+def delete_book_by_id(book_id: str):
+
+    print("[ Log ] Delete Book Id: ", book_id)
+
+    delete_message = f"Delete book id {book_id} is complete !! "
+    response = {"status": "ok", "data": delete_message}
+    return JSONResponse(content=response, status_code=200)
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=3001, reload=True)

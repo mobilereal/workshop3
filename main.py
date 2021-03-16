93d7d62f4bb8e6f2c3c4818a357ec4783b8f4305
mobilereal <suchanat.l@ku.th>
2021-03-16 16:38:03 +0700

    add book

--- main.py
+++ main.py
@@ -29,5 +29,51 @@ def get_query_parameter(start: int = 0, limit: int = 0):
     )
 
 
+@app.get("/books")
+def get_books():
+    dict_books = [
+        {
+            "book_id": 1,
+            "book_name": "harry potter and philosopher's stone",
+            "page": 223,
+        },
+        {
+            "book_id": 2,
+            "book_name": "harry potter and the chamber of secrets",
+            "page": 251,
+        },
+        {
+            "book_id": 3,
+            "book_name": "harry potter and prisoner ofazkaban",
+            "page": 251,
+        },
+    ]
+
+
+@app.get("/books{book_id}")
+def get_books_by_id(book_id: int):
+    dict_books = [
+        {
+            "book_id": 1,
+            "book_name": "harry potter and philosopher's stone",
+            "page": 223,
+        },
+        {
+            "book_id": 2,
+            "book_name": "harry potter and the chamber of secrets",
+            "page": 251,
+        },
+        {
+            "book_id": 3,
+            "book_name": "harry potter and prisoner ofazkaban",
+            "page": 251,
+        },
+    ]
+    
+    book_filter = list(filter(lambda book : book ["book_id"] = book_id,dict_books))
+    result = book_filter[0] if len(book_filter) > 0 else []
+    return JSONResponse(content={"status": "ok", "data": dict_books}, status_code=200)
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=3001, reload=True)

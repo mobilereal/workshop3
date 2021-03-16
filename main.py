f0c841de8f2f425d9e0ca5f25c2e75a36ed84b1d
mobilereal <suchanat.l@ku.th>
2021-03-16 17:02:54 +0700

    add class

--- main.py
+++ main.py
@@ -34,45 +34,83 @@ def get_books():
     dict_books = [
         {
             "book_id": 1,
-            "book_name": "harry potter and philosopher's stone",
+            "book_name": "Harry Potter and Philosopher's Stone",
             "page": 223,
         },
         {
             "book_id": 2,
-            "book_name": "harry potter and the chamber of secrets",
+            "book_name": "Harry Potter and the Chamber of Secrets",
             "page": 251,
         },
         {
             "book_id": 3,
-            "book_name": "harry potter and prisoner ofazkaban",
+            "book_name": "Harry Potter and the Prisoner of Azkaban",
             "page": 251,
         },
     ]
 
+    response = {"status": "ok", "data": dict_books}
 
-@app.get("/books{book_id}")
+    return JSONResponse(content=response, status_code=200)
+
+
+@app.get("/books/{book_id}")
 def get_books_by_id(book_id: int):
     dict_books = [
         {
             "book_id": 1,
-            "book_name": "harry potter and philosopher's stone",
+            "book_name": "Harry Potter and Philosopher's Stone",
             "page": 223,
         },
         {
             "book_id": 2,
-            "book_name": "harry potter and the chamber of secrets",
+            "book_name": "Harry Potter and the Chamber of Secrets",
             "page": 251,
         },
         {
             "book_id": 3,
-            "book_name": "harry potter and prisoner ofazkaban",
+            "book_name": "Harry Potter and the Prisoner of Azkaban",
             "page": 251,
         },
     ]
-    
-    book_filter = list(filter(lambda book : book ["book_id"] = book_id,dict_books))
-    result = book_filter[0] if len(book_filter) > 0 else []
-    return JSONResponse(content={"status": "ok", "data": dict_books}, status_code=200)
+
+    book_filter = {}
+    for book in dict_books:
+        if book["book_id"] == book_id:
+            book_filter = book
+
+    response = {"status": "ok", "data": book_filter}
+
+    return JSONResponse(content=response, status_code=200)
+
+
+class createBookPayload(BaseModel):
+    id: str
+    name: str
+    page: int
+
+
+@app.post("/books")
+def create_books(req_body: createBookPayload):
+    req_body_dict = req_body.dict()
+
+    id = req_body_dict["id"]
+    name = req_body_dict["name"]
+    page = req_body_dict["page"]
+
+    print("[ Log ] name", id)
+    print("[ Log ] name", name)
+    print("[ Log ] page", page)
+
+    book = {
+        "id": id,
+        "name": name,
+        "page": page,
+    }
+
+    response = {"status": "ok", "data": book}
+
+    return JSONResponse(content=response, status_code=201)
 
 
 if __name__ == "__main__":
